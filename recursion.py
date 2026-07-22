7d5ff11adf0acd857b4e05c1203acab0b05c9989
misssswa1123 <missswapnali23@gmail.com>
2026-07-22 21:18:52 +0530

    Recursion

---
+++ recursion.py
@@ -0,0 +1,36 @@
+# def fact(num):
+
+#     if num==0 or num==1:
+#         print(num)
+#         return 1    
+#     print(num)
+#     return num*fact(num-1)
+# print("Factorial=",fact(5))
+
+# def fab(n):
+#     if n==0:
+#         return 0
+#     if n==1:
+#         return 1
+    
+#     return fab(n-1)+fab(n-2)
+
+# print(fab(5)
+
+# def fab(n):
+#     a=0
+#     b=1
+#     # print(a)
+#     # print(b)
+#     for i in range(10):
+       
+#         print(a)
+#         c=a+b
+#         a=b
+#         b=c
+
+# fab(10)
+
+
+
+
