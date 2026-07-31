7217296b23a2605a594ef4e40537025021860011
misssswa1123 <missswapnali23@gmail.com>
2026-08-01 00:54:47 +0530

    random module

---
+++ randommodule.py
@@ -0,0 +1,25 @@
+import random as r
+
+# op=r.randrange(0,2)
+# print(op)
+# print(op1)
+op1=r.randint(0,2)
+
+list=['rock','paper','scissor']
+# # draw,winyou,winai=0]
+# draw=0
+# winai=0
+# winyou=0
+# for i in range(0,len(list)):
+#     print(f'{i}.',list[i])
+
+# option=int(input("Enter the your option(0,1,2)="))
+# if list[option]==list[op1]:
+#     print("Draw")
+#     draw+=1
+
+# else:
+#     print('more...')
+
+# print(r.choices(list,2))
+
